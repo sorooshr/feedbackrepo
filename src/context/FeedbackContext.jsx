c07d609b7c5dae21451687ccb01e3cf14baa7770
unknown <s.soroosh.r.n@gmail.com>
2022-04-19 16:36:26 +0430

    the update and delete now connected to the backend

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -19,7 +19,7 @@ export const FeedbackProvider = ({children}) => {
   //fetch feedback
   const fetchFeedback = async () => {
       const response = await fetch(
-          `http://localhost:5000/feedback?_sort=id$_order=desc`
+          `/feedback?_sort=id$_order=desc`
       )
       const data = await response.json()
       
@@ -28,22 +28,40 @@ export const FeedbackProvider = ({children}) => {
   }
 
   //feedback deleting
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
    if (window.confirm("Are you sure you want to deletet this feedback?")) {
+      await fetch(`/feedback/${id}`, {method: 'DELETE'})
       setFeedback(feedback.filter((item) => item.id !== id));
     }
 }
 
   // adding feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async(newFeedback) => {
+    const response = await fetch(`/feedback`, {
+        method: "post",
+        headers:{
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
+    setFeedback([data, ...feedback])
   };
 
   //edit feedback
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+           const response = await fetch(`/feedback/${id}`, {
+               method: 'PUT',
+               headers: {
+                   'Content-Type': 'application/json' 
+               },
+               body: JSON.stringify(updItem)
+            })
+
+            const data = await response.json()
       setFeedback(
-          feedback.map((item) => (item.id === id ? {...item, ...updItem} : item))
+          feedback.map((item) => (item.id === id ? {...item, ...data} : item))
       )
   }
 
